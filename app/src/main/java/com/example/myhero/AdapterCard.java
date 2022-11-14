38cf26326aa23eae988b5ee4071885d1ba95fb57
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2022-11-14 11:17:44 +0700

    New

---
+++ app/src/main/java/com/example/myhero/AdapterCard.java
@@ -0,0 +1,16 @@
+package com.example.myhero;
+
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterCard
+{
+    public class ClassViewHolder extends RecyclerView.ViewHolder
+    {
+        public ClassViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myhero/ModelHero.java
@@ -0,0 +1,30 @@
+package com.example.myhero;
+
+public class ModelHero
+{
+    private String name, about, photo;
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getAbout()
+    {
+        return about;
+    }
+    public void setAbout(String about)
+    {
+        this.about = about;
+    }
+    public String getPhoto()
+    {
+        return photo;
+    }
+    public void setPhoto(String photo)
+    {
+        this.photo = photo;
+    }
+}
