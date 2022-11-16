c71fd5f20ed29a689528439e3107ead487d20b89
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2022-11-16 15:18:35 +0700

    Update

--- app/src/main/java/com/example/myhero/AdapterCard.java
+++ app/src/main/java/com/example/myhero/AdapterCard.java
@@ -1,16 +1,56 @@
 package com.example.myhero;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
 
-public class AdapterCard
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder>
 {
+    private ArrayList<ModelHero> dataHero;
+    public AdapterCard(ArrayList<ModelHero> dataHero)
+    {
+        this.dataHero = dataHero;
+    }
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new ClassViewHolder(varView);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position)
+    {
+        ModelHero hero = dataHero.get(position);
+        holder.tvName.setText(hero.getName());
+        holder.tvAbout.setText(hero.getAbout());
+        Glide
+                .with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .centerCrop()
+                .into(holder.ivPhoto);
+    }
+    @Override
+    public int getItemCount()
+    {
+        return dataHero.size();
+    }
     public class ClassViewHolder extends RecyclerView.ViewHolder
     {
+        ImageView ivPhoto;
+        TextView tvName, tvAbout;
         public ClassViewHolder(@NonNull View itemView)
         {
             super(itemView);
+            ivPhoto = itemView.findViewById(R.id.iv_photo);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvAbout = itemView.findViewById(R.id.tv_about);
         }
     }
 }
--- app/src/main/java/com/example/myhero/MainActivity.java
+++ app/src/main/java/com/example/myhero/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.myhero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
+    private RecyclerView rvHero;
+    private ArrayList<ModelHero> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvHero = findViewById(R.id.rv_hero);
+        rvHero.setHasFixedSize(true);
+        data.addAll(DataHero.takeDataHero());
+        showDataCard();
+    }
+    private void showDataCard()
+    {
+        rvHero.setLayoutManager(new LinearLayoutManager(this));
+        AdapterCard varAdapterCard = new AdapterCard(data);
+        rvHero.setAdapter(varAdapterCard);
     }
 }
